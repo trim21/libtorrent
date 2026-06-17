f7febb57906fd3656c03d4617a54aa5e96521632
trim21 <i@trim21.me>
2026-06-17 23:45:45 +0800

    fix: add visibility(default) to diag functions

--- src/net/event_fd.cc
+++ src/net/event_fd.cc
@@ -169,7 +169,7 @@ static constexpr int kDiagCbBuckets = 64;
 static diag_cb_bucket g_cb_buckets[kDiagCbBuckets];
 static std::atomic<int> g_cb_next_bucket;
 
-void
+__attribute__((visibility("default"))) void
 __diag_track_callback(const char* target, const char* caller, bool is_intr) {
   int idx{};
   for (int i = 0; i < kDiagCbBuckets; i++) {
@@ -195,7 +195,7 @@ __diag_track_callback(const char* target, const char* caller, bool is_intr) {
   }
 }
 
-void
+__attribute__((visibility("default"))) void
 __diag_dump_callbacks() {
   FILE* fp = fopen("/tmp/eventfd_diag.log", "a");
   if (!fp) return;
